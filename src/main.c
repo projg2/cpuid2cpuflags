fbc7fdccd1233361eb93392b12a13d9c228eb755
Michał Górny <mgorny@gentoo.org>
2016-07-14 20:54:45 +0200

    Add command-line option parsing

--- src/main.c
+++ src/main.c
@@ -8,12 +8,49 @@
 #endif
 #include "platforms.h"
 
+#include <getopt.h>
+#include <stdio.h>
+
 /* x86.c */
 void print_x86();
 
+struct option long_options[] = {
+	{ "help", no_argument, 0, 'h' },
+	{ "version", no_argument, 0, 'V' },
+	{ 0, 0, 0, 0 }
+};
+
+const char* usage = "Usage: %s [options]\n"
+	"\n"
+	"Options:\n"
+	"  -h, --help               print this help message\n"
+	"  -V, --version            print program version\n";
+
 int main(int argc, char* argv[])
 {
-	/* TODO: command-line parsing */
+	int opt;
+
+	while ((opt = getopt_long(argc, argv, "hV", long_options, 0)) != -1)
+	{
+		switch (opt)
+		{
+			case 'h':
+				printf(usage, argv[0]);
+				return 0;
+			case 'V':
+				puts(PACKAGE_STRING);
+				return 0;
+			case '?':
+				fprintf(stderr, usage, argv[0]);
+				return 1;
+		}
+	}
+	if (optind != argc)
+	{
+		fprintf(stderr, "%s: unexpected position parameter\n", argv[0]);
+		fprintf(stderr, usage, argv[0]);
+		return 1;
+	}
 
 #ifdef CPUID_X86
 	print_x86();
